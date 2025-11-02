a2162ffc0a1283d8bdc104ef9a1abc22d8d5fc0a
Dhruval Padia <padiadhruval@gmail.com>
2025-11-02 19:00:27 +0530

    feat: final try 2

---
+++ progression.py
@@ -0,0 +1,15 @@
+import imageio
+import os
+
+image_dir = "outputFinal"   
+output_gif = "progress2.gif"
+
+files = sorted(
+    [f for f in os.listdir(image_dir) if f.endswith(".png")],
+    key=lambda x: int(''.join(filter(str.isdigit, x)))  
+)
+
+images = [imageio.imread(os.path.join(image_dir, f)) for f in files]
+
+imageio.mimsave(output_gif, images, duration=0.15)  # 0.15s per frame
+print(f"GIF saved as {output_gif}")
